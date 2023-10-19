04ece683120ac27618d37ffe78ca58862a586a52
SeongjinOliver <yoosngj@gmail.com>
2023-10-19 09:55:17 +0900

    한가지 테스트는 한가지 목적만 가지고 테스트

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -4,7 +4,46 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ProductTypeTest {
+
+    /**
+     * 한가지 테스트에서는 한가지 목적에서만 테스트를 해야한다.
+     * 아래 처럼 하는것이 아닌 아래 두가지 테스로 분리해서 테스트를 해야한다.
+     */
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockTypeFaultExample() {
+        // given
+//        ProductType givenType = ProductType.HANDMADE;
+        ProductType[] productTypes = ProductType.values();
+
+        for (ProductType productType : productTypes) {
+            if (productType == ProductType.HANDMADE) {
+                // when
+                boolean result = ProductType.containsStockType(productType);
+
+                // then
+                assertThat(result).isFalse();
+            }
+
+            if (productType == ProductType.BAKERY || productType == ProductType.BOTTLE) {
+                // when
+                boolean result = ProductType.containsStockType(productType);
+
+                // then
+                assertThat(result).isTrue();
+            }
+        }
+
+
+//        // when
+//        boolean result = ProductType.containsStockType(givenType);
+//
+//        // then
+//        assertThat(result).isFalse();
+    }
 
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
     @Test
