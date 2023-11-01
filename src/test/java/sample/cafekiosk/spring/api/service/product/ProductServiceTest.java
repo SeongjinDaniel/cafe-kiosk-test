b8831c61adf80dcaea7ca34ee5cc453de9999b01
SeongjinOliver <yoosngj@gmail.com>
2023-11-01 09:53:12 +0900

    Add restdoc dependency on gradle

--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -2,10 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.IntegrationTestSupport;
-import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -17,7 +14,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
 class ProductServiceTest extends IntegrationTestSupport {
