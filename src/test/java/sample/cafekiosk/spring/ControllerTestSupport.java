09bdf81317f79ebe242148568619e87f111eda0e
SeongjinOliver <yoosngj@gmail.com>
2024-05-12 12:19:44 +0900

    Add comment

--- src/test/java/sample/cafekiosk/spring/ControllerTestSupport.java
+++ src/test/java/sample/cafekiosk/spring/ControllerTestSupport.java
@@ -10,6 +10,9 @@ import sample.cafekiosk.spring.api.controller.product.ProductController;
 import sample.cafekiosk.spring.api.service.order.OrderService;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 
+/**
+ * MockMvc와 같이 사용을 하려면 SpringBootTest 보다 좀더 Controller, Service 빈들만 띄울수 있는 좀 더 가벼운 테스트 annotation
+ */
 @WebMvcTest(controllers = {
     OrderController.class,
     ProductController.class
